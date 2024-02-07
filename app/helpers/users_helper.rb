d0654782cf3db94be935ad31435daee96d9db405
Addie Mongelli <addie.mongelli@stash.com>
2024-02-07 16:48:21 -0600

    basic signup form

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+    def gravatar_for(user)
+        gravatar_url = "https://secure.gravatar.com/avatar/9717af0dabd5aebb7527cd4afaf2d9e8"
+        image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
 end
