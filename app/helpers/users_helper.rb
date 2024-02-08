b1ad11f7c1cd838104434fd4caf100dcd3828d58
Addie Mongelli <addie.mongelli@stash.com>
2024-02-08 15:45:02 -0600

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
     def gravatar_for(user)
-        gravatar_url = "https://secure.gravatar.com/avatar/9717af0dabd5aebb7527cd4afaf2d9e8"
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravatar_url = "https://secure.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af"
         image_tag(gravatar_url, alt: user.name, class: "gravatar")
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   get 'about' =>    'static_pages#about'
   get 'contact' =>  'static_pages#contact'
   get 'signup' =>   'users#new'
+  # resources :users basically just makes these for you
+  # get 'users/:id' =>  'users#show'
+  # post 'users/' =>  'users#create'
+  # put 'users/:id' => 'users#update'
+  # delete 'users/:id' => 'users#destroy'
   resources :users
 end
