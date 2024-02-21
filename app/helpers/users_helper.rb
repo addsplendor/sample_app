49f078a5359f1b65a6892413a626662472a8cd7a
Addie Mongelli <addie.mongelli@stash.com>
2024-02-21 12:01:32 -0600

    seeded data for users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
-    def gravatar_for(user)
+    def gravatar_for(user, options = { size: 80 })
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-        gravatar_url = "https://secure.gravatar.com/avatar/b58996c504c5638798eb6b511e6f49af"
+        size = options[:size]
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
         image_tag(gravatar_url, alt: user.name, class: "gravatar")
     end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,6 +7,11 @@ class UsersControllerTest < ActionDispatch::TestCase
     @other_user = users(:addie)
   end
 
+  test "should redirect index when not logged in"
+    get :index
+    assert_redirect_to login_url
+  end
+
   test "should get new" do
     get :new
     assert_response :success
