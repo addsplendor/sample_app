023c9db179afe64747be9cedaa54277c7f05ad3b
Addie Mongelli <addie.mongelli@stash.com>
2024-02-29 13:14:56 -0600

    post creation and delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
-class User < ActiveRecord::Base 
-   attr_accessor :remember_token, :activation_token
+class User < ActiveRecord::Base
+   has_many :microposts, dependent: :destroy
+   attr_accessor :remember_token, :activation_token, :reset_token
    before_save :downcase_email 
    before_create :create_activation_digest
    validates :name, presence: true, length: { maximum: 50}
@@ -17,18 +18,18 @@ class User < ActiveRecord::Base
          BCrypt::Password.create(string, cost: cost)
    end
 
-   #Returns a random token.
+   # Returns a random token.
    def User.new_token
       SecureRandom.urlsafe_base64
    end
 
-   #Remembers a user in the db for use in persistent sessions.
+   # Remembers a user in the db for use in persistent sessions.
    def remember
       self.remember_token = User.new_token
       update_attribute(:remember_digest, User.digest(remember_token))
    end
 
-   #Forgets a user.
+   # Forgets a user.
    def forget
       update_attribute(:remember_digest, nil)
    end
@@ -39,11 +40,34 @@ class User < ActiveRecord::Base
       update_attribute(:activated_at, Time.zone.now)
    end
 
-   #Sends activation email.
+   # Sends activation email.
    def send_activation_email
       UserMailer.account_activation(self).deliver_now
    end
 
+   # Sets the password reset attributes.
+   def create_reset_digest
+      self.reset_token = User.new_token
+      update_attribute(:reset_digest, User.digest(reset_token))
+      update_attribute(:reset_sent_at, Time.zone.now)
+   end
+
+     # Sends password reset email.
+     def send_password_reset_email
+      UserMailer.password_reset(self).deliver_now
+   end
+
+   # Returns true if a password reset has expired.
+   def password_reset_expired?
+      reset_sent_at < 2.hours.ago
+   end
+
+   # Defines a proto-feed.
+   # See "Following users" for the full implementation.
+   def feed
+      Micropost.where("user_id = ?", id)
+   end
+   
    # Returns true if the given token matches the digest.
    def authenticated?(attribute, token)
       digest = self.send("#{attribute}_digest")
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'account_activations/edit'
   get 'sessions/new'
   get 'users/new'
@@ -18,4 +20,8 @@ Rails.application.routes.draw do
   # delete 'users/:id' =>  'users#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+  get '/microposts', to: 'static_pages#home'
 end
